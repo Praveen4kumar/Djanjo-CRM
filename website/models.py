c09e060f76c39e5e1d726c8ed2546002c44d3bf6
Praveen4kumar <praveenmani246@gmail.com>
2024-02-18 17:09:29 +0530

    Final Project

--- website/models.py
+++ website/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Record(models.Model):
+	created_at = models.DateTimeField(auto_now_add=True)
+	first_name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
+	email = models.CharField(max_length=50)
+	phone = models.CharField(max_length=50)
+	address = models.CharField(max_length=50)
+	city = models.CharField(max_length=50)
+	state = models.CharField(max_length=50)
+	pincode = models.CharField(max_length=50)
